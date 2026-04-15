017e488d7424d1e9fa5c466b8e107369d442d463
CarlosGardelG <cgga1@aluno.ifal.edu.br>
2026-04-15 11:01:24 -0300

    Começo da tela de aulas (AppBar)

--- lib/aulas.dart
+++ lib/aulas.dart
@@ -10,6 +10,50 @@ class Aulas extends StatefulWidget {
 class _AulasState extends State<Aulas> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Aulas')));
+    return Scaffold(appBar: AppBar(
+      //prefixIcon: Icon(Icons.whatshot),
+      //prefixIcon: Icon(Icons.monetization_on),
+      //suffixIcon: Icon(Icons.wallet_giftcard_sharp),     ),
+      backgroundColor: Color(0xFF4F2B82),
+      elevation: 0,
+      flexibleSpace: Container( //flexiblespace é basicamente o conteúdo que sera colcoado na appbar(tipo child). O primeiro contrainer vai conter a cor secundária
+          child: Align( //O Align é um widget usado pra posicionar um filho dentro de outro espaço. É tipo usar o stack para conseguir colocar duas informações no mesmo espaço
+          alignment: Alignment.bottomCenter, //posiciona no centro na horizontal
+          child: Container(//vamos colocar as informações da appbar aqui
+            height: 60,
+            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 13), //adiconando margens
+            decoration: BoxDecoration(
+                color: Color(0xFFA770F4),
+                borderRadius: BorderRadius.circular(15) //box decoration pra deixar circular
+            ),
+
+              child: Center(
+                child: Row(
+                  children: [
+                    Stack(
+                      children: [
+                        Icon(Icons.whatshot_outlined, color: Colors.red, size: 37),
+                        Icon(Icons.whatshot, color: Colors.deepOrangeAccent, size: 35),
+                        Icon(Icons.whatshot_outlined, color: Colors.amber, size: 34), //besteiragem pra fazer ficar bonitinho
+                        Positioned(//para dar espaço do icone
+                            child: Text('------------------------------------------------------------------------------')
+                        ),
+
+                      ],
+                    ),
+
+
+                  ],
+                )
+            ),
+
+          )
+        )
+
+      ),
+
+
+      ),
+    );
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projetogaejoju/amigos.dart';
 import 'package:projetogaejoju/aulas.dart';
 import 'package:projetogaejoju/mascote.dart';
 import 'package:projetogaejoju/profile.dart';
@@ -18,6 +19,7 @@ class _HomePageState extends State<HomePage> {
   List pages = [
     Aulas(),
     Revisao(),
+    Amigos(),
     Mascote(),
     Profile()
   ];
@@ -27,10 +29,11 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       body: pages[selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Color(0xFF8D5CD1),
           currentIndex: selectedIndex, //o index da barra (cada posição do item) fica correspondente a variavel que declaramos
           showSelectedLabels: true, //coloca o nome dos itens nao selecionados
           unselectedItemColor: Colors.grey, //coloca cor nos itens que nao tiverem selecionados
-          selectedItemColor: Colors.blue, //colcoa uam cor especifica para o item selecionado
+          selectedItemColor: Colors.white, //colcoa uam cor especifica para o item selecionado
           type: BottomNavigationBarType.fixed, //tira o movimento na hora de apertar em uma item
           onTap: (index){ //a troca do item fica por toque
             setState(() {
@@ -41,6 +44,7 @@ class _HomePageState extends State<HomePage> {
           items: [
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Aulas'),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Revisao'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Amigos'),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Mascote'),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Profile')
           ]),
