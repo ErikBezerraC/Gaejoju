d070861f79f8fd19f88a2467770fa23a085f6505
CarlosGardelG <cgga1@aluno.ifal.edu.br>
2026-04-16 17:46:31 -0300

    Tela de profile pronta

--- lib/profile.dart
+++ lib/profile.dart
@@ -30,15 +30,27 @@ class Profile extends StatefulWidget {
         ),
       ),
       backgroundColor: Color(0xFF4F2B82),
-      body: Center(
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.start, //jogando o widget pra cima
+      body: ListView(
+        children:[
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start, //jogando o widget pra cima
 
-             children: [
-               buildContainer(url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVJdld9FG1S3HXfELeAofZ3MWauAcBxBtwWQ&s',nome:'Marina Sena',gmail:'marinasena@gmail.com'),
-               //quantidade de strikes, conquistas,
-            ],
-        ),
+              children: [
+                buildContainer(url:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVJdld9FG1S3HXfELeAofZ3MWauAcBxBtwWQ&s',nome:'Marina Sena',gmail:'marinasena@gmail.com'),
+                SizedBox(height: 20,),
+                containerDias(),
+                SizedBox(height: 15,),
+                containerConquistas(),
+                SizedBox(height: 15,),
+                containerPorcent(),
+                //quantidade de strikes, conquistas,
+              ],
+            ),
+          ),
+        ]
+
+
       ),
     );
   }
@@ -89,25 +101,218 @@ buildContainer({required String url, required String nome, required String gmail
             fontSize: 18,
             color: Colors.white,
             fontWeight: FontWeight.bold,
-          )
-        ),
-        SizedBox(
-          height:20,
-        ),
-        Container(
-          width: 350,
-          height: 270, //tamanho do container
-          decoration: BoxDecoration(
-            color: Color(0xFFA770F4), //adicionando cor de fundo
-            borderRadius: BorderRadius.circular(8), //arredondando as bordas
-          )
-        ),
-          SizedBox(
-            height:20,
           ),
+        ),
+
         ]
     ),
   )
   );
 
 }
+
+containerDias(){
+  return Container(
+    width: 350,
+    height: 105,//tamanho do container
+    alignment: Alignment.topLeft,//colocar o texto o canto superior esquerdo
+    decoration: BoxDecoration(
+      color: Color(0xFFA770F4), //adicionando cor de fundo
+      borderRadius: BorderRadius.circular(8), //arredondando as bordas
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+         Padding(
+          padding: EdgeInsets.only(top: 10, left: 15),
+          child: Text(
+            'Dias Consecutivos',
+            style: GoogleFonts.comicNeue(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+        ),
+        Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only( left: 12),
+              child: Stack(
+                children: [
+                  Icon(Icons.whatshot_outlined, color: Colors.red, size: 50),
+                  Icon(Icons.whatshot, color: Colors.deepOrangeAccent, size: 48),
+                  Icon(Icons.whatshot_outlined, color: Colors.amber, size: 47), //besteiragem pra fazer ficar bonitinho
+                ],
+              ),
+
+            ),
+
+            Padding(
+              padding: EdgeInsets.only( left: 5),
+                child: Text(
+                'Selo de',
+                style: GoogleFonts.comicNeue(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  ),
+                )
+            ),
+            Padding(
+                padding: EdgeInsets.only( left: 5),
+                child: Text(
+                  '30!',
+                  style: GoogleFonts.comicNeue(
+                    fontSize: 25,
+                    color: Colors.amber,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+            ),
+          ],
+        ),
+      ],
+    ),
+
+  );
+}
+containerConquistas(){
+  return Container(
+    width: 350,
+    height: 105,//tamanho do container
+    alignment: Alignment.topLeft,//colocar o texto o canto superior esquerdo
+    decoration: BoxDecoration(
+      color: Color(0xFFA770F4), //adicionando cor de fundo
+      borderRadius: BorderRadius.circular(8), //arredondando as bordas
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 10, left: 15),
+          child: Text(
+            'Suas Conquistas ',
+            style: GoogleFonts.comicNeue(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+        ),
+        Row(
+          children: [
+            Padding(
+                padding: EdgeInsets.only( left: 12),
+                child: Icon(Icons.emoji_events, color: Colors.amber, size: 47,)
+
+            ),
+
+            Padding(
+                padding: EdgeInsets.only( left: 5),
+                child: Text(
+                  'Você Tem',
+                  style: GoogleFonts.comicNeue(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+            ),
+            Padding(
+                padding: EdgeInsets.only( left: 5),
+                child: Text(
+                  '55!',
+                  style: GoogleFonts.comicNeue(
+                    fontSize: 25,
+                    color: Colors.amber,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+            ),
+          ],
+        ),
+      ],
+    ),
+
+  );
+}
+containerPorcent(){
+  return Container(
+    width: 350,
+    height: 105,//tamanho do container
+    alignment: Alignment.topLeft,//colocar o texto o canto superior esquerdo
+    decoration: BoxDecoration(
+      color: Color(0xFFA770F4), //adicionando cor de fundo
+      borderRadius: BorderRadius.circular(8), //arredondando as bordas
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 10, left: 15),
+          child: Text(
+            'Porcentagem do Curso',
+            style: GoogleFonts.comicNeue(
+              fontSize: 25,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+
+        ),
+        Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Stack(
+                children: [
+                  Container(
+                    height: 50,
+                    width: 250,
+                    decoration: BoxDecoration(
+                      color: Colors.transparent, //adicionando cor de fundo
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: Colors.black54, // cor da borda
+                        width: 6,
+                      ),
+                    ) ,
+                  ),
+                  Container(),
+                  Padding(
+                    padding: EdgeInsets.only(left: 5,top: 5),
+                    child: Stack(
+                      children: [
+                        Container(
+                          height: 41,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: Colors.yellow, //adicionando cor de fundo
+                            borderRadius: BorderRadius.circular(12),
+                          ) ,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(width: 10,),
+            Text('80%',style: GoogleFonts.comicNeue(
+              fontSize: 25,
+              color: Colors.amber,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+          ],
+        ),
+      ],
+    ),
+
+  );
+}
+
+
