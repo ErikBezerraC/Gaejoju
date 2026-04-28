02a939f9b2fab81e84d83208bbf8191d35dedf9a
jamylle <jsss2@aluno.ifal.edu.br>
2026-04-28 11:34:59 -0300

    Coloquei todas as telas na minha branch para apresentar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:projetogaejoju/config.dart';
 import 'package:projetogaejoju/home_page.dart';
 import 'package:projetogaejoju/loja.dart';
 import 'package:projetogaejoju/variaveis_revisao.dart';
 
 void main() {
   //runApp(MaterialApp(home: VariaveisRevisao()));
-  runApp(MaterialApp(home: Loja()));
+  //runApp(MaterialApp(home: Loja()));
   //runApp(MaterialApp(home: HomePage()));
+  runApp(MaterialApp(home: Config()));
 }
-
-
