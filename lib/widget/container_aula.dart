67d70f06027a8dde47475e16603a5a6f6bd6f623
barbosajuliana13 <contato.julianabarbosa321@gmail.com>
2026-06-16 17:24:02 -0300

    comecei a desenvolver as melhorias da tela de aulas

---
+++ lib/widget/container_aula.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:projetogaejoju/domain/aulas.dart';
+
+class ContainerAula extends StatefulWidget {
+
+  const ContainerAula({super.key});
+
+  @override
+  State<ContainerAula> createState() => _ContainerAulaState();
+}
+
+class _ContainerAulaState extends State<ContainerAula> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
