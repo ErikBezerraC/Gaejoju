7219766f2211ed5267ee345915e0c4423a1722e0
barbosajuliana13 <contato.julianabarbosa321@gmail.com>
2026-09-15 11:29:08 -0300

    Create Livros class with properties and constructor

---
+++ lib/domain/livros.dart
@@ -0,0 +1,13 @@
+class Livros {
+  final String titulo;
+  final String autor;
+  final String pdfUrl;
+  final String capaUrl;
+
+  Livros({
+    required this.titulo,
+    required this.autor,
+    required this.pdfUrl,
+    required this.capaUrl,
+  });
+}
