17c58f2dc90776b235b7f110eed0116ef6d3f8a1
barbosajuliana13 <contato.julianabarbosa321@gmail.com>
2026-09-15 10:19:43 -0300

    Refactor Aulasvid class to VideoApi with Dio integration

--- lib/api/aulasvid_api.dart
+++ lib/api/aulasvid_api.dart
@@ -1,9 +1,21 @@
-import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:projetogaejoju/domain/videos.dart';
 
-class Aulasvid{
-  final Videoslist videoslist = Videoslist();
+class VideoApi {
+  final Dio dio = Dio();
 
-  VideoList ({super.key});
+  Future<List<Videos>> listarVideos() async {
+    final resposta = await dio.get(
+      'https://my-json-server.typicode.com/barbosajuliana13/fake_api_Gaejoju/VIDEOS',
+    );
 
+    List<Videos> listarVideos = [];
 
+    for (var json in resposta.data) {
+      Videos video = Videos.fromJson(json);
+      listarVideos.add(video);
+    }
+
+    return listarVideos;
+  }
 }
